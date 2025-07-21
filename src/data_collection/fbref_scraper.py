db3ba33b86b3a806571e209fd87a4da079c5b8b7
Chadrick <chadrickclarkea@gmail.com>
2025-07-21 06:23:26 -0400

    "attempt to find table"

--- src/data_collection/fbref_scraper.py
+++ src/data_collection/fbref_scraper.py
@@ -7,8 +7,28 @@ page= requests.get(URL)
 #print(page.text)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+soup_tag_div = soup.div
 
-print(soup.body)
-#print(soup.title)
-#tables = soup.find(id='div_stats_standard')
-#rint(tables)
+# print(soup_tag_div.find_parents(
+#     'table', attrs="table container is_setup"
+#     )
+#     )
+
+# print(soup.find_parents(name='table',attrs='table container is_setup'))
+
+# print(soup.select('div > table'))
+
+
+#find all tags = "table"
+# print(soup.find_all(
+#     name='div', 
+#     #attrs= "min_width sortable stats_table shade_zero now_sortable sticky_table eq2 re2 le2",
+#     ))
+
+#print(soup.find_all('table',id='stats_standard'))
+
+
+#print(soup_tag_div['id':"wrap"])
+print(soup)
+#print(soup.table)
+#print(type(soup.table))
