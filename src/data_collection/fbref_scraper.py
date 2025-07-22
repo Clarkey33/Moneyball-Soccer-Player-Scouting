77bf615126c31ef9dfd5fabc98075c761337a00a
Chadrick <chadrickclarkea@gmail.com>
2025-07-22 06:20:42 -0400

    "<1-- html comment tag issue found"

--- src/data_collection/fbref_scraper.py
+++ src/data_collection/fbref_scraper.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import requests
 
 URL = 'https://fbref.com/en/comps/9/stats/Premier-League-Stats'
@@ -7,28 +7,21 @@ page= requests.get(URL)
 #print(page.text)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-soup_tag_div = soup.div
-
-# print(soup_tag_div.find_parents(
-#     'table', attrs="table container is_setup"
-#     )
-#     )
-
-# print(soup.find_parents(name='table',attrs='table container is_setup'))
-
-# print(soup.select('div > table'))
 
 
-#find all tags = "table"
-# print(soup.find_all(
-#     name='div', 
-#     #attrs= "min_width sortable stats_table shade_zero now_sortable sticky_table eq2 re2 le2",
-#     ))
 
-#print(soup.find_all('table',id='stats_standard'))
+table=soup.find(
+        name='div',
+        attrs={
+                #'class':'table_container',
+                'id':'div_stats_standard'
+                }
+                )
 
+for content in table(x=lambda x: isinstance(x,Comment)):
+    content.extract()
 
-#print(soup_tag_div['id':"wrap"])
-print(soup)
-#print(soup.table)
-#print(type(soup.table))
+#print(soup.prettify())
+print(table)
+#<div class="table_container" id="div_stats_standard">
+
