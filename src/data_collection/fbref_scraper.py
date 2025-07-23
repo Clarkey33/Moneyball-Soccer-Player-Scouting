90061b813ab5a972c27e29fd56a549fd7ec0855a
Chadrick <chadrickclarkea@gmail.com>
2025-07-23 08:48:31 -0400

    "updates"

--- src/data_collection/fbref_scraper.py
+++ src/data_collection/fbref_scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup, Comment
 import requests
+import re
 
 URL = 'https://fbref.com/en/comps/9/stats/Premier-League-Stats'
 
@@ -9,19 +10,39 @@ page= requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 
+# re.sub(pattern,"",soup)
+#soup = soup.replace('<!--',"")
 
-table=soup.find(
+div_tables=soup.find(
         name='div',
         attrs={
-                #'class':'table_container',
-                'id':'div_stats_standard'
+                #'class':'table_container is_setup',
+                'id':'all_stats_standard'
                 }
                 )
+table_stats=None
+for element in div_tables.children:
+    if isinstance(element, Comment):
+        table_stats=element
 
-for content in table(x=lambda x: isinstance(x,Comment)):
-    content.extract()
 
-#print(soup.prettify())
-print(table)
+print(type(table_stats))
+print(table_stats)
+
+
+#print(type(div_tables.table.string))
+
+# table = str(div_tables)
+# pattern =r"<[!]--|-[-]>"
+# table = re.sub(pattern,"",table)
+
+
+#print(table)
+
+
+
+
+
+#print(div_tables.prettify())
+#print(div_tables)
 #<div class="table_container" id="div_stats_standard">
-
