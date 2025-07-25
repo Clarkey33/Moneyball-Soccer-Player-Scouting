0e612ff6dd57e45fba832549d3f3782c2546a75b
Chadrick <chadrickclarkea@gmail.com>
2025-07-25 06:22:29 -0400

    "tested-other-leagues_success"

--- src/data_collection/fbref_scraper.py
+++ src/data_collection/fbref_scraper.py
@@ -4,7 +4,15 @@ import re
 import pandas as pd
 from io import StringIO
 
-URL = 'https://fbref.com/en/comps/9/stats/Premier-League-Stats'
+#Portugal
+#URL = 'https://fbref.com/en/comps/32/2024-2025/stats/2024-2025-Primeira-Liga-Stats'
+#Brasil
+#URL = 'https://fbref.com/en/comps/24/2024/stats/2024-Serie-A-Stats' 
+#Belgium
+#URL = 'https://fbref.com/en/comps/37/2024-2025/stats/2024-2025-Belgian-Pro-League-Stats'
+
+#Netherlands
+URL = 'https://fbref.com/en/comps/23/2024-2025/stats/2024-2025-Eredivisie-Stats'
 
 page= requests.get(URL)
 
@@ -28,6 +36,7 @@ table_str= StringIO(str(table))
 
 df_stats = pd.read_html(table_str, index_col=1,header=1)[0]
 print(df_stats.head())
+print(f'\n{df_stats.info()}')
 print(f'\ncolumn names:\n{df_stats.columns.tolist()}')
 
 
