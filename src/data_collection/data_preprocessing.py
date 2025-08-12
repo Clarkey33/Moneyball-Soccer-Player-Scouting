9fb0dcb9b8c58f68b9a700689a860fd273ab8a58
Chadrick <chadrickclarkea@gmail.com>
2025-08-12 06:26:14 -0400

    "data_preprocessing_script_updates"

--- src/data_collection/data_preprocessing.py
+++ src/data_collection/data_preprocessing.py
@@ -6,21 +6,6 @@ from io import StringIO
 import os
 from pathlib import Path
 
-#Portugal
-#URL = 'https://fbref.com/en/comps/32/2024-2025/stats/2024-2025-Primeira-Liga-Stats'
-#Brasil
-#URL = 'https://fbref.com/en/comps/24/2024/stats/2024-Serie-A-Stats' 
-#Belgium
-#URL = 'https://fbref.com/en/comps/37/2024-2025/stats/2024-2025-Belgian-Pro-League-Stats'
-
-#Netherlands
-# URL = 'https://fbref.com/en/comps/23/2024-2025/stats/2024-2025-Eredivisie-Stats'
-
-# page= requests.get(URL)
-# print(type(page.content))
-
-#saved_html_pages_path = "../../data/raw"
-#saved_html_pages_path = "/home/on3b3ar/projects/moneyball-wolves-cunha-replacement/data/raw"
 
 script_file_path =Path(__file__).resolve()
 script_file_dir = script_file_path.parent
@@ -35,29 +20,42 @@ output_dir = "../../data/processed"
 page = os.path.join(saved_html_pages_path,saved_html_pages[0])
 pages= open(page)
 
-#create soup object
-# with open(str(saved_html_pages[0])) as f:
-#     soup = BeautifulSoup(f)
-#print(soup)
+def parse_table_from_html(
+        html_page, 
+        div_id:str) -> str:
+    
+    soup = BeautifulSoup(
+        html_page.read(),
+        features= 'html.parser'
+        )
+    
+    div_container = None
+    try: 
+        if soup.find('div',div_id):
+            div_container = soup.find('div',div_id)
+        else:
+            print("Contents not found for tag id :<div id{div_locator}")
+
+    except Exception as e:
+        print("f{e}")
+
+    
 
 
-soup = BeautifulSoup(pages.read(), features= 'html.parser')
-#print(soup)
+    #parse data from comment tag
+    comment = soup.find(string=re.compile('<table class='))
 
-#parse data from comment tag
-comment = soup.find(string=re.compile('<table class='))
+    #create new soup object of comment class
+    new_soup= BeautifulSoup(comment, 'html.parser')
+    #print(new_soup)
 
-#create new soup object of comment class
-new_soup= BeautifulSoup(comment, 'html.parser')
-print(new_soup)
+    #parse required data only
+    table = new_soup.find(
+        name='table',
+        #attrs={'id':'stats_standard'}
+        )
 
-#parse required data only
-table = new_soup.find(
-    name='table',
-    #attrs={'id':'stats_standard'}
-    )
-
-table_str= StringIO(str(table))
+    table_str= StringIO(str(table))
 #print(type(table_str))
 
 df_stats = pd.read_html(table_str, index_col=1,header=1)[0]
@@ -67,7 +65,16 @@ print(f'\ncolumn names:\n{df_stats.columns.tolist()}')
 
 
 
-
+#parse html from table
+"""
+1. load saved html page from ../../data/raw [input]
+2. create soup object with html page
+3. search soup oject for <div id= ''; id string [input]
+4. search for <table></table>; if found parse
+    a. If not found; check and parse table from comment tag
+    b. if no table found in either 4/5 return none and move on to next html doc
+5. return parsed table as str 
+"""
 
 
 
