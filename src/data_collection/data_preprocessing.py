13b845b16c6154fc1587ee648387d43f50c1157c
Chadrick <chadrickclarkea@gmail.com>
2025-08-18 20:17:58 -0400

    "refactoring-data_preprocessing script"

--- src/data_collection/data_preprocessing.py
+++ src/data_collection/data_preprocessing.py
@@ -15,73 +15,58 @@ from pathlib import Path
 
 
 def parse_table_from_html(
-        html_path:str, 
-        div_id:str) -> str:
-    
-    """
-1. load saved html page from ../../data/raw [input]
-2. create soup object with html page
-3. search soup oject for <div id= ''; id string [input]
-4. search for <table></table>; if found parse
-    a. If not found; check and parse table from comment tag
-    b. if no table found in either 4/5 return none and move on to next html doc
-5. return parsed table as str 
-"""
+        html, 
+        div_id:str):
 
-    with open(html_path,'r',encoding='utf-8') as f:
-        html_content = f.read()
+    # with open(html_path,'r',encoding='utf-8') as f:
+    #     html_content = f.read()
     
     soup = BeautifulSoup(
-        html_content,
+        html,
         features= 'html.parser'
         )
-    
-    div_data = None
-    parse_comments=None
-    table_data = None
-    table_data_comment= None
+    print(soup)
+    if not soup:
+        print(f"Soup object not created properly")
+        return None
+
 
     if not soup.find('div',div_id):
-        print(f"div not found!")
-        return f"There was no div with id({div_id} found: {table_data}"
+        print(f"div not found with id: {div_id}, table_found: {table_data}")
+        
+    if not soup.find('div',class_='placeholder'):
+        return None
     
     else:
         print(f'div found')
-        div_data= soup.find('div',div_id)
+    
+    div_data= soup.find('div',div_id)
+    print(div_data)
+    try:
         print("Searching for table..")
-        try: 
-            table_data = div_data.find('table')
-            table_data_comment = soup.find(string=re.compile('<table class='))
-
-            if table_data and table_data_comment:
-                print(f"\nparsing table from {table_data}\n")
-                table_data_str= StringIO(str(table_data))
-
-                print(f"\nparsing table from {table_data_comment}\n")
-                parse_comments = soup.find(string=re.compile('<table class='))
-                new_soup = BeautifulSoup(parse_comments, 'html.parser')
-                table_data_comment_= new_soup.find(name='table')
-                table_data_comment_str= StringIO(str(table_data_comment_))
-
-                return table_data_str, table_data_comment_str
-            
-            elif table_data:
-                print("Contents found for tag id : <div id={div_locator}")
-                table_data_str= StringIO(str(table_data))
-                print("Desired table found!")
-                return table_data_str
-            
-            elif table_data_comment:
-                print("Contents not found for tag id : <div id={div_locator}")
-                print("checking if hidden by comment tag '<!--' ..")
-                parse_comments = soup.find(string=re.compile('<table class='))
-                new_soup = BeautifulSoup(parse_comments, 'html.parser')
-                table_data_comment_= new_soup.find(name='table')
-                table_data_comment_str= StringIO(str(table_data_comment_))
-                print("Desired table found!")
+        table_data = div_data.find('table')
+        if table_data:
+            table_data_str= StringIO(str(table_data))
+            print(f"Table found!\n")
+            return table_data_str
+        elif not table_data:
+            #check if hidden by comment tags
+            print(f"Checking if table hidden by comment tags..")
+            parse_comments = soup.find(string=re.compile('<table class='))
+            parse_comments_soup = BeautifulSoup(parse_comments,'html.parser')
+            table_data_comment = parse_comments_soup.find(name='table')
+            if table_data_comment:
+                table_data_comment_str= StringIO(str(table_data_comment))
+                print(f"Table found !")
                 return table_data_comment_str
-        except Exception as e:
-            print(f" Failed to parse table from div: {e}")
+            else:
+                print(f"No table found!")
+                return None    
+        else:
+            print(f"no table found in div!: {div_id}")
+            return None
+    except Exception as e:
+        print(f" Failed to parse table from div: {e}\n")
 
     
 
--- src/data_preprocessing_script.py
+++ src/data_preprocessing_script.py
@@ -1,5 +1,4 @@
 from data_collection.data_preprocessing import parse_table_from_html
-from data_collection.data_collection import years_to_scrape, league_config, stat_tables, construct_filepath
 import os 
 from pathlib import Path
 
@@ -10,88 +9,46 @@ project_root = script_file_dir.parent
 html_pages_dir=project_root/"data"/"raw"
 html_pages_filename_list = os.listdir(html_pages_dir) # ->list
 
-html_pages_filepath = os.path.join(html_pages_dir,html_pages_filename_list)
-#pages= open(page)
-
-stats_name_list = [
-    'standard', 'shooting', 'keeper',
-    'passing','keeper_adv','passing_types',
-    'gca','defense','possession',
-    'playing_time','misc'
-              ]
 
 
 
-# f"{season}_{safe_league_name}_{stat_type}.html"
-# -> safe league name is made with the key value 
-#idea: create a foor loop years scrape , league name, stat type similar to data collection
-# check file path for save html is it is stat type standard.. scrape both team and player stats
-#if stat type not standard only scrape player stats
-stat_val=None
-for year in years_to_scrape:
-    for league_name, league_values in league_config.items():
-        season_string = ''
-        if league_values.get("season") == "single_year":
-            season_string = year
-        elif league_values.get("seaspm") == "cross_year":
-            season_string = f"{year-1}-{year}"
-        else:
-            print("no season available")
-            continue 
-        for stat in stats_name_list:
-            if stat.find('_'):
-                stat_val=stat.replace('_',"")
-            else:
-                continue
-
-            html_file_path = construct_filepath(
-                output_dir=html_pages_dir,
-                season=season_string,
-                league_name=league_name,
-                stat_type=stat_val
-            )
-
-            if not os.path.isfile(html_file_path):
-                continue
-
-            else:
-                if stat == "standard":
-                    table_players_stats = parse_table_from_html(
-                        html_page=html_pages_filepath,
-                        div_id=stat
-                        )
-
-#refractor code to simplify logic
-#return to original approach; do not reconstruct the file path
-#use the list of files already captured in code
-#split path to extract stat type , seasons etc
-#div id is stat type; two types have '_' separator
-#only need squad and player tables for standard
-#div id tempplate:  id= all_stats_squads_<stat_type> | id= all_stats_<stat_type>
+for html_page in html_pages_filename_list:
+    html_filepath = os.path.join(html_pages_dir,html_page)
+    html_name = html_filepath.replace('.html','')
+    html_tags= html_name.split('_')
+    stat_type_tag = html_tags[-1]
 
 
+    if stat_type_tag == 'stats':
+        stat_type_tag='standard'
+        div_id_team = f'all_stats_squads_{stat_type_tag}'
+        div_id_players = f'all_stats_{stat_type_tag}'
+
+        data_table_team = parse_table_from_html(html_path=html_filepath,
+                                            div_id=div_id_team
+                                            )
+        data_table_players = parse_table_from_html(html_path=html_filepath,
+                                                    div_id= div_id_players
+                                                    )
+        
+    elif stat_type_tag == 'keepersadv':
+        stat_type_tag = 'keepers_adv'
+        div_id_players = f'all_stats_{stat_type_tag}'
+        data_table_players = parse_table_from_html(html_path=html_filepath,
+                                                    div_id= div_id_players
+                                                    )
+        
+    elif stat_type_tag == 'playingtime':
+        stat_type_tag = 'playing_time'
+        div_id_players = f'all_stats_{stat_type_tag}'
+        data_table_players = parse_table_from_html(html_path=html_filepath,
+                                                    div_id= div_id_players
+                                                    )
+    else:
+        div_id_players = f'all_stats_{stat_type_tag}'
+        data_table_players = parse_table_from_html(html_path=html_filepath,
+                                                    div_id= div_id_players
+                                                    )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for stat_name in stats_name_list:
-    div_id = f"all_stats_{stat_name}"
-    print(div_id)
-
-    table_players_stats = parse_table_from_html(html_page=html_pages_filepath,
-                                                div_id=div_id
-                                                )
-    
-    print(f"Table data type:  {type(table_players_stats)}")
+    print(f"Data retrieved for {html_page}.\n")
